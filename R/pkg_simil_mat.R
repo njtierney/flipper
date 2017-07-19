43d3faabb54893a9b36d30dff9fd4ca3de0b18e2
mpadge <mark.padgham@email.com>
2017-07-20 00:37:14 +0200

    improve verbose output for pkg_simil_mat

--- R/pkg_simil_mat.R
+++ R/pkg_simil_mat.R
@@ -3,20 +3,26 @@
 #' Generate a similarity matrix between packages based on their textual
 #' descriptions
 #'
+#' @param verbose If \code{TRUE}, display progress messages
+#'
 #' @export
-pkg_simil_mat <- function ()
+pkg_simil_mat <- function (verbose = FALSE)
 {
+    if (verbose)
+        message ('Extracting package information from CRAN ...')
     pkgs <- tools::CRAN_package_db()
     pkg_txt <- apply (cbind (pkgs$Title, pkgs$Description), 1, paste,
                       collapse = " ")
     pkg_txt <- gsub ("\n", " ", pkg_txt)
     names (pkg_txt) <- pkgs$Package
     pkg_corpus <- quanteda::corpus (pkg_txt)
-    #summary (pkg_corpus)
     pkg_dfm <- quanteda::dfm (pkg_corpus,
                               remove = quanteda::stopwords ("english"),
                               stem = TRUE,
                               remove_punct = TRUE,
-                              verbose = TRUE)
+                              verbose = verbose)
+    if (verbose)
+        message (paste0 ('Generating similarity matrix between ',
+                         'package descriptions ...'))
     quanteda::textstat_simil (pkg_dfm, names (pkg_txt))
 }
