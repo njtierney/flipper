2b7936bf0549d3ed8b1b106f2f7c9378ed43c41c
mpadge <mark.padgham@email.com>
2018-03-22 23:03:55 +0100

    invisible return pkg tibble, re: #12

--- R/flip.R
+++ R/flip.R
@@ -9,13 +9,26 @@ flip <- function (phrase)
 {
     packages <- text_to_pkgs (phrase)
     print_pkg (packages$pkg_names [1])
-    y <- menu( c("Yes", "No", "Maybe"), title="Good package?")
+    y <- menu( c("Yes", "No", "Maybe"), title = "Good package?")
+
+    pout <- NULL
+    if (y == 1)
+        pout <- packages$pkg_names [1]
 
     i <- 2
     while (y %in% c (1, 3) & i <= nrow (packages))
     {
         print_pkg (packages$pkg_names [i])
-        y <- menu( c("Yes", "No", "Maybe"), title="Good package?")
+        y <- menu( c("Yes", "No", "Maybe"), title = "Good package?")
+        if (y == 1)
+            pout <- c (pout, packages$pkg_names [i])
         i <- i + 1
     }
+
+    pkgs <- m_get_cran_pkgs () # memoised here
+    pkgs <- cbind (pkgs$Package, pkgs$Title, pkgs$Description)
+    pkgs <- tibble::as.tibble (pkgs)
+    names (pkgs) <- c ("Package", "Title", "Description")
+
+    invisible (pkgs [match (pout, pkgs$Package), ])
 }
